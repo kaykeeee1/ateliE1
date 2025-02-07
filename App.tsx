79f7d56daa72c6b63b33383d834c8f0d5d37f2ab
kaykeeee1 <kaykeevangelista0@gmail.com>
2025-02-07 11:46:13 -0300

    refacture

--- App.tsx
+++ App.tsx
@@ -11,4 +11,5 @@ export default function App() {
 
 const styles = StyleSheet.create({
  
+  
 });
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,31 +9,35 @@ import {
 
 import { style } from "./style";
 import Logo from '../../assets/new_logo3.png'
+import fundo from '../../assets/carreteis.png'
 
-export default function login (){
-return(
-   <View style={style.container}>
-    <View style={style.boxTop}>
-        <Text>top</Text>
-        <Image
-        source={Logo}
-        style={style.logo}
-        resizeMode="contain"
-        />
-        <Text style={style.text}>Bem vindo ao atelie Connect</Text>
-    </View>
-    <View style={style.boxMid}>
-    <Text style={style.titleInput}>ENDEREÇO DE E-EMAIL</Text>
-    <TextInput>
-        <Text><Text>Endereço de e-mail</Text></Text>
-    </TextInput >
-        </View >
-        <View style={style.boxBottom}>
-        <Text style={style.titleInput}>Digite sua senha</Text>
-        
-    </View>
-   </View>
-   
-)
+export default function login() {
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}>
+                <Image
+                    source={Logo}
+                    style={style.logo}
+                    resizeMode="contain"
+                />
+                <Text style={style.text}>Bem vindo ao atelie Connect</Text>
+            </View>
+            <View style={style.boxMid}>
+                <Text style={style.titleInput}>ENDEREÇO DE E-EMAIL</Text>
+                <View style={style.boxInput}>
+                    <TextInput />
+                    <Text></Text>
+                </View>
+                <Text style={style.titleInput}>SENHA</Text>
+            </View>
+            <View style={style.boxBottom}>
+                <Image source={fundo}
+                >
+                    
+                </Image>
+            </View>
+        </View>
+
+    )
 
 }
